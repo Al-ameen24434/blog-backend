15dc634e525b1d5ffbd985db8e58091f29fea858
al-ameen24434 <alameenabdulrahmon@gmail.com>
2025-12-26 22:53:40 +0100

    added dto for post

---
+++ src/post/dto/create-post.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsString()
+  @MinLength(10)
+  content: string;
+
+  @IsOptional()
+  @IsString()
+  slug?: string;
+
+  @IsOptional()
+  @IsString()
+  thumbnail?: string;
+
+  @IsBoolean()
+  published: boolean;
+
+  @IsInt()
+  authorId: number;
+}
