79184118e7b28e7595bbcce5133cc80e2c95d685
al-ameen24434 <alameenabdulrahmon@gmail.com>
2025-12-29 23:53:29 +0100

    post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -12,15 +12,15 @@ export class PostService {
     return `This action returns all post`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} post`;
+  // }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
+  // update(id: number, updatePostDto: UpdatePostDto) {
+  //   return `This action updates a #${id} post`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} post`;
+  // }
 }
