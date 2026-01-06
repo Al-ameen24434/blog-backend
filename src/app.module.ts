77349243bc73b974786cf0048dce2860e00d0350
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-06 10:33:08 +0100

    added .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule, PostModule],
+  imports: [
+    PrismaModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env', // 👈 FORCE load
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
