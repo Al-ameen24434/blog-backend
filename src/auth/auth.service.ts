a7d88c4470200a5bf71b19f4ca14213d24ee8ea8
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-09 23:24:03 +0100

    auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { UnauthorizedException } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async register(dto: RegisterDto) {
+    const hash = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: dto.name,
+        email: dto.email,
+        password: hash,
+      },
+    });
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (!user || !(await bcrypt.compare(dto.password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.signToken(user.id, user.email);
+  }
+
+  private signToken(userId: number, email: string) {
+    return {
+      access_token: this.jwt.sign({ sub: userId, email }),
+    };
+  }
+}
