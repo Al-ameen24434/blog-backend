b826efa308edd86e9edc48dcc244373bd4af26cb
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-11 22:52:15 +0100

    commentt entity

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,33 @@
-export class Comment {}
+import { ApiProperty } from '@nestjs/swagger';
+import { UserEntity } from '../../user/entities/user.entity';
+import { PostEntity } from '../../post/entities/post.entity';
+
+export class CommentEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty()
+  postId: number;
+
+  @ApiProperty()
+  authorId: number;
+
+  @ApiProperty({ type: UserEntity })
+  author?: UserEntity;
+
+  @ApiProperty({ type: PostEntity })
+  post?: PostEntity;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  constructor(partial: Partial<CommentEntity>) {
+    Object.assign(this, partial);
+  }
+}
