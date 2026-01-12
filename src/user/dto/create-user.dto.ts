5f7bf33aa3648ec5db2a0c7b2458406ba0e0853d
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-12 23:18:27 +0100

    user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,40 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsEnum,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../../../generated/prisma/client';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
+  @MinLength(2)
   name: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  bio?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+
+  @ApiProperty()
+  @IsString()
   @MinLength(6)
   password: string;
+
+  @ApiProperty({ enum: Role, default: Role.USER })
+  @IsEnum(Role)
+  @IsOptional()
+  role?: Role;
 }
