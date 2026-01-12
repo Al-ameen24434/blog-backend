839123f0d9a7b1cf46015d96f702096303e453fb
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-12 23:17:05 +0100

    user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  bio: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  avatar: string | null;
+
+  @Exclude()
+  password: string;
+
+  @ApiProperty()
+  role: string;
+
+  @Exclude()
+  refreshToken: string | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+}
