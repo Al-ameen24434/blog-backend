60d71bf19bb3fc11db06c56512fba8d2d42a4a9b
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-12 23:17:22 +0100

    tag entity

--- src/tag/entities/tag.entity.ts
+++ src/tag/entities/tag.entity.ts
@@ -1,1 +1,13 @@
-export class Tag {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class TagEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  constructor(partial: Partial<TagEntity>) {
+    Object.assign(this, partial);
+  }
+}
