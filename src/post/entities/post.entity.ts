b78a188033cb5bae77c6dfb699a40615ca35959b
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-13 21:31:55 +0100

    post entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,49 @@
-export class Post {}
+import { ApiProperty } from '@nestjs/swagger';
+import { UserEntity } from '../../user/entities/user.entity';
+import { CommentEntity } from '../../comment/entities/comment.entity';
+import { TagEntity } from '../../tag/entities/tag.entity';
+
+export class PostEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty({ required: false, nullable: true })
+  slug: string | null;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  thumbnail: string | null;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  authorId: number;
+
+  @ApiProperty({ type: UserEntity })
+  author?: UserEntity;
+
+  @ApiProperty({ type: [CommentEntity] })
+  comments?: CommentEntity[];
+
+  @ApiProperty({ type: [TagEntity] })
+  tags?: TagEntity[];
+
+  @ApiProperty()
+  likesCount?: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  constructor(partial: Partial<PostEntity>) {
+    Object.assign(this, partial);
+  }
+}
