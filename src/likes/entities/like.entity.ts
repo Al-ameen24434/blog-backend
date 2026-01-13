103d3085057d43c09a8b4d63074e71fab2540576
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-01-13 21:33:37 +0100

    likes entity

--- src/likes/entities/like.entity.ts
+++ src/likes/entities/like.entity.ts
@@ -1,1 +1,20 @@
-export class Like {}
+// src/likes/entities/like.entity.ts
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LikeEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  userId: number;
+
+  @ApiProperty()
+  postId: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  constructor(partial: Partial<LikeEntity>) {
+    Object.assign(this, partial);
+  }
+}
